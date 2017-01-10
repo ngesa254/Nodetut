29ef94bbdef5eb75a7cfe9fac9a69ea30be9d276
Ngesa Marvin <marvinngesa@gmail.com>
2017-01-10 14:06:45 +0300

    Update module2.js

--- modules/module2.js
+++ modules/module2.js
@@ -1,23 +1,5 @@
 //An easier way of exporting modules using Javascript Objects
 
-
-/*function myFunction1() {
-	// 
-	console.log('Fuck Off');
-}
-
-
-var mySting1 = 'Fuck!';
-
-
-module.exports.myFunction1 = myFunction1;
-module.exports.mySting1 = mySting1;
-*/
-
-
-
-
-
 module.exports = {
 
 myFunction1 : function () {
