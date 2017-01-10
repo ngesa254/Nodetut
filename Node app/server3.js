5657a99f0e380292af0c5e17724c1fb90b881f07
Ngesa Marvin <marvinngesa@gmail.com>
2017-01-10 12:21:18 +0300

    Add files via upload

---
+++ Node app/server3.js
@@ -0,0 +1,13 @@
+ var http = require ('http');
+
+
+ var server = http.createServer (function (req, res) {
+ 	// body...
+
+ 	res.writeHead(200, {'Content-Type': 'text/plain'});
+ 	res.write('Meet the most awesome GDE around!');
+ 	res.end();
+ }).listen(9000, function() {
+ 	// body...
+ 	console.log('server listening to port 9000');
+ });
