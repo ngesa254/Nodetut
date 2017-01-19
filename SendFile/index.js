40b912ccedf8c948842c67a16fa7411c5d6ae80b
Ngesa Marvin <marvinngesa@gmail.com>
2017-01-19 22:03:29 +0300

    Update index.js

--- SendFile/index.js
+++ SendFile/index.js
@@ -1,1 +1,22 @@
 
+/Importing required modules
+
+var express = require ("express");
+var app = express();
+
+//Creating requests
+
+app.get("/", function (req, res) {
+
+	//res.send('Hello Ninja!');
+	res.sendFile(__dirname +'/index.html');
+
+	
+});
+
+
+//Creating the server
+app.listen(2222, function () {
+	
+	console.log("server is now listening to port: 2222");
+});
